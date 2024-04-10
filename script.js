3f680ded81e6d808914cd0e33e164b1f7001656d
Markedia Hinds <mhinds@pursuit.org>
2024-04-10 13:08:56 -0400

    M.O.K.A Creating Circles Incomplete

--- script.js
+++ script.js
@@ -3,15 +3,14 @@ const body = document.querySelector('body')
 
 button.addEventListener('click', (e) => {
     e.preventDefault
-    const newContainer = document.createElement('div')
-    newContainer.setAttribute = ("style", "width: 85%; height: 100%; border: 5px solid black;")
-    newContainer.setAttribute = ('class', 'container')
-    body.append('newContainer')
+    // const newContainer = document.createElement('div')
+    // newContainer.setAttribute = ("style", "width: 85%; height: 100%; border: 5px solid black;")
+    // newContainer.setAttribute = ('class', 'container')
+    // body.append('newContainer')
     
     let container = document.querySelector('.container')
 
     let newCircle = document.createElement('div')
-    container.setAttribute('class', '')
     newCircle.setAttribute('class', 'circle')
     newCircle.setAttribute = ("style", "height: 40px; width: 40px; background-color: getRandomColor(); border-radius: 50%;")
     container.append(newCircle);
